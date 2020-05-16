a4b3b18de55ab394f86269b08b848d943e12c2a3
Leo Cui <Basic.Bear@outlook.com>
2020-05-16 19:34:50 -0400

    new format

---
+++ Easy/Easy.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode_CS.Easy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_CS.Easy
+{
+    [TestClass()]
+    public class Easy_Test
+    {
+        [TestMethod()]
+        public void Reverse_Test()
+        {
+            List<(int, int)> cases = new List<(int, int)>();
+
+            cases.Add((123, 321));
+            cases.Add((452, 254));
+            cases.Add((-392, -293));
+            cases.Add((1239, 9321));
+            cases.Add((15336469, 96463351));
+
+            LC07_ReverseInteger student = new LC07_ReverseInteger();
+
+            int dataIn, expectedOut, receivedOut;
+            
+            foreach ((int, int) c in cases)
+            {
+                (dataIn, expectedOut) = c;
+                receivedOut = student.Reverse(dataIn);
+                Assert.AreEqual(expectedOut,receivedOut);
+            }
+
+        }
+
+
+
+
+        [TestMethod()]
+        public void numJewelsInStones_Test()
+        {
+            // Arrange
+
+            Dictionary<(String, String), int> cases = new Dictionary<(string, string), int>();
+            cases.Add(("aA", "aAAbbbb"), 3);
+            cases.Add(("z", "ZZ"), 0);
+
+            LC0771_JewelsNStones student = new LC0771_JewelsNStones();
+
+            String J;
+            String S;
+            int output;
+
+            foreach ((String, String) k in cases.Keys)
+            {
+                // Act
+                (J, S) = k;
+                output = student.numJewelsInStones(J, S);
+
+                // Assert
+                Assert.AreEqual(cases[k], output, 0, $"Got:{output}|Expected:{cases[k]}");
+            }
+        }
+    }
+}
---
+++ Medium/Medium.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace LeetCode_CS.Medium
+{
+    [TestClass()]
+    public class Medium
+    {
+        [TestMethod()]
+        public void Rob_Test()
+        {
+
+            // Arrange
+            List<(int[], int)> cases = new List<(int[], int)>();
+            cases.Add((new int[] { 2, 3, 2 }, 3));
+            cases.Add((new int[] { 1, 2, 3, 1 }, 4));
+
+            LC0213_HouseRobber2 student = new LC0213_HouseRobber2();
+
+            int[] caseInput;
+            int caseAnswer;
+            int receivedAnswer;
+            foreach ((int[], int) c in cases)
+            {
+                // Act
+                (caseInput, caseAnswer) = c;
+                receivedAnswer = student.Rob(caseInput);
+
+                // Assert
+                Assert.AreEqual(caseAnswer, receivedAnswer);
+
+            }
+
+
+        }
+    }
+}
